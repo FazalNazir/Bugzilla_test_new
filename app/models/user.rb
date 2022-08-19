978f5c3ba2e0fc0b0e8f0cae3155d8fcf187fecc
AsadDevsinc <asad.yasin@devsinc.com>
2022-08-19 15:42:53 +0500

    Devise implementations with mailer * Implemented devise * User signup * User login * Customized devise views using bootstrap * Verified all implementations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_and_belongs_to_many :projects, dependent: :destroy
   enum type: { Manager: 1, Developer: 2, QualityAssurance: 3 }
 
