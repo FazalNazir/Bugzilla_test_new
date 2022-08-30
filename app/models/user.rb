bfad6c5b38232f61fa1600644706535e86c280af
AsadDevsinc <asad.yasin@devsinc.com>
2022-08-30 14:53:49 +0500

    Implemented model level validations * Removed unnecessary lines of code * Implemented model level validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_and_belongs_to_many :projects, dependent: :destroy
   enum type: { Manager: 1, Developer: 2, QualityAssurance: 3 }
 
-  # validates :name, :email, :password, presence: true
-  # validates_format_of :email, with: /\A([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})\z/i
+  validates :name, :email, :password, presence: true
+  validates :email, format: { with: /\A([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})\z/i }
+  validates :name, length: { maximum: 50,
+                             too_long: '%<count>s characters is the maximum allowed' }
 end
